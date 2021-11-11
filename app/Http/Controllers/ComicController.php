a6930a36dea6607594c277f47d4b1106600322ae
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-11 13:28:28 +0100

    Aggiunta seeder

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -58,9 +58,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Comic $comic)
+    public function show($id)
     {
-        // $comic = Comic::find($id);
+        $comic = Comic::find($id);
 
         return view("comics.show", compact("comic"));
     }
